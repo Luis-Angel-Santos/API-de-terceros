b0dabc5755b97d552d987cd04061b741e8cf373e
Luis Angel <santosangel748@gmail.com>
2020-09-15 16:21:13 -0500

    Primeros pasos con la APP(Clima)

---
+++ Clima/js/app.js
@@ -0,0 +1,13 @@
+const containe=document.querySelector('.container');
+const resultado=document.querySelector('#resultado');
+const formulario=document.querySelector('#formulario');
+
+window.addEventListener('load', () =>{
+    formulario.addEventListener('submit', buscarclima);
+})
+
+function buscarclima(e){
+    e.preventDefault();
+    
+
+}
