66a22cd86b6533b174959987d0efa99359f31b2f
Luis Angel <santosangel748@gmail.com>
2020-09-15 16:35:58 -0500

    Validando el formulario

--- Clima/js/app.js
+++ Clima/js/app.js
@@ -1,4 +1,4 @@
-const containe=document.querySelector('.container');
+const container=document.querySelector('.container');
 const resultado=document.querySelector('#resultado');
 const formulario=document.querySelector('#formulario');
 
@@ -8,6 +8,31 @@ window.addEventListener('load', () =>{
 
 function buscarclima(e){
     e.preventDefault();
-    
 
+    const ciudad=document.querySelector('#ciudad').value;
+    const pais=document.querySelector('#pais').value;
+
+    if(ciudad==='' || pais===''){
+        mostarError('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostarError(mensaje){
+    const alerta=document.querySelector('.bg-red-100');
+
+        if(!alerta){
+            const alerta=document.createElement('div');
+
+            alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+            alerta.innerHTML=`
+                <strong class="font-bold">Error!</strong>
+                <span class="block">${mensaje}</span> 
+            `;
+            container.appendChild(alerta);
+
+            setTimeout(()=>{
+                alerta.remove();
+            }, 5000);
+        }
 }
